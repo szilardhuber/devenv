8c10dae95b3add74dfe53726e01be918ab28af19
szilardhuber <szilardhuber@Szilards-MacBook-Air.local>
2015-10-15 09:21:42 +0200

    Fix username

--- 003-git.sh
+++ 003-git.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git config --global push.default simple
-git config --global user.name "Szilard Huber"
+git config --global user.name szilardhuber
 git config --global user.email szilard.huber@gmail.com
