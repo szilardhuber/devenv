919b5262271ea23bcd7ec9e352fd96c10b9ffd19
szilardhuber <szilard.huber@gmail.com>
2024-01-21 08:35:38 +0100

    Remove some more unused stuff

--- 002-nvm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install 0.12
--- 003-git.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git config --global push.default simple
-git config --global user.name szilardhuber
-git config --global user.email szilard.huber@gmail.com
